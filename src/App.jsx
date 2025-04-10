2b1e3267888217bb8a029218e430c552c3aa292b
ffsasa <ngocan2003krp@gmail.com>
2025-04-10 13:46:01 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './index.css'; 
+
+function App() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Hello TailwindCSS 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
