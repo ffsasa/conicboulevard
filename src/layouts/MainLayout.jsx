32cd915ac106eb1f3bc58216c05ca0a2ec0e7821
ffsasa <ngocan2003krp@gmail.com>
2025-04-10 17:41:50 +0700

    Update navbar and hero

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,13 +2,16 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 const MainLayout = ({ children }) => {
-  return (
-    <>
-      <Navbar />
-      <main className="p-4">{children}</main>
-      <Footer />
-    </>
-  );
-};
-
-export default MainLayout;
+    return (
+      <>
+        <Navbar />
+        <main>
+          {children}
+        </main>
+        <Footer />
+      </>
+    );
+  };
+  
+  export default MainLayout;
+  
